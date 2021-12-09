499cefbfc415b3d03d7ce0b037e237fe7f3b5a14
MMartynamajchrzak <mmartynamajchrzak@wp.pl>
2021-12-09 02:50:44 +0100

    add fitness function, mutations and inversions

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import random
 from math import sqrt
 
 
-def greedy_algorithm(file_name):
-    coord = []
+def read(file_name):
+    with open(file_name) as file:
+        coord = [tuple(i.split()[0:3]) for i in file.readlines()[6:]]
+
+    return coord
+
+
+def greedy_algorithm(coord):
     final_distance = 0
 
-    # result = dict()
-    with open(file_name) as file:
-        text = file.readlines()[6:]
-        for i in text:
-            coord.append(tuple(i.split()[0:3]))
-
     # choosing one random element from which to start
-    r = random.randint(1, len(coord) + 1)
+    r = random.randint(1, len(coord))
     new_coord = [r]
 
     # list of numbers representing indexes from remaining cities
@@ -42,14 +42,120 @@ def greedy_algorithm(file_name):
     result = f"list of indexes of cities sorted by the closest distance {new_coord} " \
              f"\nGive the total distance of {format(final_distance, '.2f')}"
 
-    return result
+    return new_coord
 
+
+# Distance
+def distance(cities, coord):
+    path = 0
+
+    for idx, item in enumerate(cities):
+        # case we return from last to first city
+        if idx == len(cities) - 1:
+
+            dist = sqrt((float(coord[cities[idx] - 1][1]) - float(coord[cities[0] - 1][1])) ** 2 +
+                        (float(coord[cities[idx] - 1][2]) - float(coord[cities[0] - 1][2])) ** 2)
+            path += dist
+        else:
+            dist = sqrt((float(coord[cities[idx+1]-1][1]) - float(coord[cities[idx]-1][1])) ** 2 +
+                        (float(coord[cities[idx+1]-1][2]) - float(coord[cities[idx]-1][2])) ** 2)
+            path += dist
+
+    return format(path, ".2f")
+
+
+# Initial population
+def population(cities, coords):
+    pop = []
+    for _ in range(89):
+        random.shuffle(cities)
+        new = list(cities)
+        pop.append(new)
+
+    for _ in range(11):
+        greedy_results = greedy_algorithm(coords)
+        pop.append(greedy_results)
+
+    return pop
+
+
+# Fitness function
+def fitness(whole_population, coord):
+    results = []
+    for idx, item in enumerate(whole_population):
+        dist = float(distance(whole_population[idx], coord))
+        results.append([dist, item])
+
+    fittest = sorted(results)[:50]
+
+    print(f"50 results with the shortest path: {fittest}")
+
+    return fittest
+
+
+# Mutations for the population
+def mutations(pop, coord):
+    print(f"Population before mutations {pop} has distance: {distance(pop, coord)}")
+    random_index = random.randint(0, len(pop)-1)
+    random_index_2 = random.randint(0, len(pop)-1)
+
+    pop[random_index], pop[random_index_2] = pop[random_index_2], pop[random_index]
+
+    print(f"Population after mutations {pop} has distance: {distance(pop, coord)}")
+
+    return pop
+
+
+def inversion(pop, coord):
+    random_index = random.randint(0, len(pop) - 1)
+    random_index_2 = random.randint(0, len(pop) - 1)
+
+    if random_index < random_index_2:
+        smaller, bigger = random_index, random_index_2
+    else:
+        smaller, bigger = random_index_2, random_index
+
+    print(f"Population before inversion {pop} has distance: {distance(pop, coord)}")
+
+    to_reverse = pop[smaller:bigger]
+    to_reverse.reverse()
+    new_pop = pop
+    del new_pop[smaller:bigger]
+    new_pop[smaller:smaller] = to_reverse
+
+    print(f"New population {new_pop} has distance: {distance(new_pop, coord)}")
+
+    return new_pop
+
+
+def main():
+    file_name = 'files/berlin11_modified.tsp'
+
+    # read file
+    file = read(file_name)
+
+    # get list of cities (by indexes) with the shortest path starting from one random city
+    cities = greedy_algorithm(file)
+
+    # create an initial population of length 100
+    pop = population(cities=cities, coords=file)
+
+    # find the best 50% of the population
+    fitness(pop, file)
+
+    # make mutations and inversions
+    mutations(cities, file)
+    inversion(cities, file)
+
+
+if __name__ == "__main__":
+    main()
 
 # FILES to test
 
-greedy_algorithm('files/ali535.tsp')
-greedy_algorithm('files/berlin11_modified.tsp')
-greedy_algorithm('files/berlin52.tsp')
+# greedy_algorithm('files/ali535.tsp')
+# greedy_algorithm('files/berlin11_modified.tsp')
+# greedy_algorithm('files/berlin52.tsp')
 # greedy_algorithm('files/fl417.tsp')
 # greedy_algorithm('files/kroA100.tsp')
 # greedy_algorithm('files/kroA150.tsp')
